d130a03e0e6cbf129c4968c82f962da4b4dde611
Jappie Klooster <jappieklooster@hotmail.com>
2019-11-30 13:20:59 +0100

    Add more cats and other things

--- makefile
+++ makefile
@@ -1,2 +1,6 @@
 run:
 	nix-shell --run "./shell.sh"
+
+schtop:
+	echo "Kill the cats with bio weapons"
+	pkill oneko
--- shell.sh
+++ shell.sh
@@ -1,4 +1,15 @@
 #! /bin/sh
 
 oneko -name 1 &!
+sleep 1
 oneko -name 2 -toname 1 &!
+sleep 1
+oneko -name 3 -toname 2 -tora &!
+sleep 1
+oneko -name 4 -toname 3 -dog &!
+sleep 1
+oneko -name 5 -toname 4 -bsd_daemon &!
+sleep 1
+oneko -name 6 -toname 5 -sakura &!
+sleep 1
+oneko -name 7 -toname 6 -tomoyo &!
