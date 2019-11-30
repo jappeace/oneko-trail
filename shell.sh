1d4b54cc93cd25c542fd6f8e7e65a15e0625ab53
Jappie Klooster <jappieklooster@hotmail.com>
2019-11-30 13:35:22 +0100

    Add colour effects

--- shell.sh
+++ shell.sh
@@ -2,14 +2,14 @@
 
 oneko -name 1 &!
 sleep 1
-oneko -name 2 -toname 1 &!
+oneko -name 2 -toname 1 -fg red &!
 sleep 1
-oneko -name 3 -toname 2 -tora &!
+oneko -name 3 -toname 2 -tora -fg blue &!
 sleep 1
-oneko -name 4 -toname 3 -dog &!
+oneko -name 4 -toname 3 -dog -bg yellow &!
 sleep 1
-oneko -name 5 -toname 4 -bsd_daemon &!
+oneko -name 5 -toname 4 -bsd_daemon -bg red &!
 sleep 1
 oneko -name 6 -toname 5 -sakura &!
 sleep 1
-oneko -name 7 -toname 6 -tomoyo &!
+oneko -name 7 -toname 6 -tomoyo -bg green &!
